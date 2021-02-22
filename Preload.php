cf8221b8221335490a1a22cfe2d387fdf1f6021a
Miguel Valentin <westerlogic@live.com>
2021-02-21 23:30:11 -0500

    new

--- Preload.php
+++ Preload.php
@@ -1,3 +1,22 @@
-<span class="percent ">0</span>
-<span class="text-strok">TEOTEC</span>
-<span class="text-fill">TEOTEC</span>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ejemplo</title>
+</head>
+
+<body>
+    <?php
+        include('index.html');
+    ?>
+    <?php
+        echo 5 + 6;
+        echo "¡Hola, soy un script de PHP!";
+    ?>
+    <span class="percent ">0</span>
+    <span class="text-strok">TEOTEC</span>
+    <span class="text-fill">TEOTEC</span>
+
+</body>
+
+</html>
